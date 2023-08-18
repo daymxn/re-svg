76e93f9624fa037e34ca27d2714be39d62d929a0
Daymon <daymxn@users.noreply.github.com>
2023-08-18 11:42:43 -0500

    Add basic JVM config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.runtime)
 }
 
 group = "com.daymxn"
@@ -8,4 +12,19 @@ version = "1.0.0"
 repositories {
     mavenCentral()
     mavenLocal()
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
 }
